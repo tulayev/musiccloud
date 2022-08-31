40688326ae6bf2048edd4a79a9691c037ac19729
Umarbek Tulaev <69952016+tulayev@users.noreply.github.com>
2022-08-31 10:11:47 +0500

    refactoring

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import App from './app/layout/App'
-import { store, StoreContext } from './app/store/store'
+import App from './layout/App'
+import { store, StoreContext } from './store/store'
 import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(
